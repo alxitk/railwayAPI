24926b01f0672f0a5de7fe01f399c701cfa0ca52
Oleksii Drenov <leshadrenev@gmail.com>
2025-10-03 16:36:28 +0300

    chore: add stations, rest framework and user apps to INSTALLED_APPS

--- railwayAPI/settings.py
+++ railwayAPI/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "user",
+    "stations",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
