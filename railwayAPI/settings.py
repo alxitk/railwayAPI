7418c408b1b26da5eace21e3ef0909adc37f8172
Oleksii Drenov <leshadrenev@gmail.com>
2025-10-03 23:50:03 +0300

    feat(stations): add custom permission and update Station views

--- railwayAPI/settings.py
+++ railwayAPI/settings.py
@@ -127,6 +127,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "user.User"
 
 REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
         "rest_framework.renderers.BrowsableAPIRenderer",
--- stations/views.py
+++ stations/views.py
@@ -2,6 +2,7 @@ from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
 
 from stations.models import Station
+from stations.permissions import IsAdminOrIfAuthenticatedReadOnly
 from stations.serializers import StationSerializer
 
 
@@ -10,3 +11,4 @@ class StationViewSet(GenericViewSet,
                      mixins.ListModelMixin,):
     queryset = Station.objects.all()
     serializer_class = StationSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
