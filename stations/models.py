f473438e9a4fd348e7d418383fd0ba26e9fedf0c
Oleksii Drenov <leshadrenev@gmail.com>
2025-10-04 16:30:10 +0300

    feat(stations): add initial models and fixture
    - Added models: Station, Route, Crew, TrainType, Train, Journey, Order, Ticket
    - Added initial_data.json fixture to populate DB with sample data
    - Created migration 0002 for new models

--- stations/models.py
+++ stations/models.py
@@ -1,6 +1,83 @@
 from django.db import models
 
+from user.models import User
+
+
 class Station(models.Model):
     name = models.CharField(max_length=100)
     latitude = models.FloatField()
     longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Station,
+        on_delete=models.CASCADE,
+        related_name="routes_from"
+    )
+    destination = models.ForeignKey(
+        Station,
+        on_delete=models.CASCADE,
+        related_name="routes_to"
+    )
+    distance = models.FloatField()
+
+    def __str__(self):
+        return f"{self.source} → {self.destination}"
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class TrainType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Train(models.Model):
+    name = models.CharField(max_length=100)
+    cargo_number = models.IntegerField()
+    places_in_cargo = models.IntegerField()
+    train_type = models.ForeignKey(TrainType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} <UNK> {self.train_type}"
+
+
+class Journey(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    train = models.ForeignKey(Train, on_delete=models.CASCADE)
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    crew = models.ManyToManyField("Crew", related_name="journeys")
+
+    def __str__(self):
+        return f"{self.route} {self.train} {self.departure_time} {self.arrival_time}"
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.created_at} {self.user}"
+
+
+class Ticket(models.Model):
+    cargo_number = models.IntegerField()
+    seat_number = models.IntegerField()
+    journey = models.ForeignKey(Journey, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.cargo_number} {self.seat_number} {self.journey}"
