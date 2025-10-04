2a3d6019b0f58b75b4e0ec46c69d32c6e94817f6
Oleksii Drenov <leshadrenev@gmail.com>
2025-10-04 16:35:43 +0300

    feat(admin): register all models in admin

--- stations/admin.py
+++ stations/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 
-from stations.models import Station
+from stations.models import (Station,
+                             Ticket,
+                             Train,
+                             TrainType,
+                             Crew,
+                             Route,
+                             Journey,
+                             Order)
 
 admin.site.register(Station)
+admin.site.register(Route)
+admin.site.register(Crew)
+admin.site.register(TrainType)
+admin.site.register(Train)
+admin.site.register(Journey)
+admin.site.register(Order)
+admin.site.register(Ticket)
