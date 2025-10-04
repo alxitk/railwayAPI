63de19084c4dee01efd7b9ab9c8194434df4780b
Oleksii Drenov <leshadrenev@gmail.com>
2025-10-05 00:09:23 +0300

    chore(admin): enhance Train admin

--- stations/admin.py
+++ stations/admin.py
@@ -9,11 +9,15 @@ from stations.models import (Station,
                              Journey,
                              Order)
 
+@admin.register(Train)
+class TrainAdmin(admin.ModelAdmin):
+    list_display = ("name", "cargo_number", "places_in_cargo", "train_type")
+    list_filter = ("train_type",)
+
 admin.site.register(Station)
 admin.site.register(Route)
 admin.site.register(Crew)
 admin.site.register(TrainType)
-admin.site.register(Train)
 admin.site.register(Journey)
 admin.site.register(Order)
 admin.site.register(Ticket)
