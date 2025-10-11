ab59b0765a9e5f38f7dcef8dc5c66af542277e30
Oleksii Drenov <leshadrenev@gmail.com>
2025-10-11 16:34:21 +0300

    feat(auth): implement token authentication for user API and migrations

---
+++ user/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from rest_framework.authtoken import views
+
+from user.views import CreateUserView, ManageUserView, CreateTokenView
+
+app_name = "user"
+
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", CreateTokenView.as_view(), name="token"),
+    path("me/", ManageUserView.as_view(), name="manage_user")
+]
