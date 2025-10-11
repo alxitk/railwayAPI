ef8ad96d23902f9389a78eeab8b658cdc35e4622
Oleksii Drenov <leshadrenev@gmail.com>
2025-10-11 17:21:34 +0300

    user: remove username field from UserSerializer and annotate full_route for doc

--- stations/models.py
+++ stations/models.py
@@ -4,9 +4,8 @@ import uuid
 from django.conf import settings
 from django.db import models
 from django.utils.text import slugify
+from drf_spectacular.utils import extend_schema_field
 from rest_framework.exceptions import ValidationError
-
-from user.models import User
 
 
 class Station(models.Model):
@@ -32,6 +31,7 @@ class Route(models.Model):
     distance = models.FloatField()
 
     @property
+    @extend_schema_field(str)
     def full_route(self):
         return f"{self.source.name} -> {self.destination.name}"
 
