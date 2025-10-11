3a35044b6ee54a70daa64901e912ba6ac0dd79ac
Oleksii Drenov <leshadrenev@gmail.com>
2025-10-11 17:15:06 +0300

    feat(settings): add DRF throttling for anonymous and authenticated users

--- railwayAPI/settings.py
+++ railwayAPI/settings.py
@@ -146,6 +146,14 @@ REST_FRAMEWORK = {
         "rest_framework.renderers.JSONRenderer",
         "rest_framework.renderers.BrowsableAPIRenderer",
     ],
+    "DEFAULT_THROTTLE_CLASSES": [
+       "rest_framework.throttling.AnonRateThrottle",
+       "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+       "anon": "100/day",
+       "user": "1000/day"
+    },
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 5
 }
